34859de605a55ca4d6a7a134d71f3274a8dba046
Vladimir_Novikov <nvs-1988@yandex.ru>
2022-05-13 17:02:32 +0300

    добавлены тесты на главную страницу, написаны тесты на каталог, написаны тесты на карточку товара

---
+++ tests/test_catalog.py
@@ -0,0 +1,116 @@
+from selenium.webdriver.common.by import By
+from exception_handler import wait_element
+
+
+# тесты на категорию Desktops
+def test_catalog_desktops_show_all(browser):
+    browser.find_element(By.LINK_TEXT, "Desktops").click()
+    browser.find_element(By.LINK_TEXT, "Show All Desktops").click()
+    browser.find_element(By.PARTIAL_LINK_TEXT, "Product Compare (0)").click()
+
+
+def test_catalog_desktops_pc(browser):
+    browser.find_element(By.LINK_TEXT, "Desktops").click()
+    browser.find_element(By.XPATH, "//a[contains(text(),'PC (0)')]").click()
+    browser.find_element(By.XPATH, "//h2[contains(text(),'PC')]")
+
+
+def test_catalog_desktops_mac(browser):
+    browser.find_element(By.LINK_TEXT, "Desktops").click()
+    browser.find_element(By.XPATH, "//a[contains(text(),'Mac (1)')]").click()
+    browser.find_element(By.XPATH, "//h2[contains(text(),'Mac')]")
+
+
+# тесты на категорию Show All Laptops & Notebooks
+def test_catalog_desktops_laptops(browser):
+    browser.find_element(By.LINK_TEXT, "Laptops & Notebooks").click()
+    browser.find_element(By.LINK_TEXT, "Show All Laptops & Notebooks").click()
+    wait_element(browser, (By.XPATH, "//p[contains(text(),'Shop Laptop feature only the best laptop deals on ')]"))
+
+
+def test_catalog_macs(browser):
+    browser.find_element(By.LINK_TEXT, "Laptops & Notebooks").click()
+    browser.find_element(By.XPATH, "//a[contains(text(),'Macs (0)')]").click()
+    browser.find_element(By.XPATH, "//p[contains(text(),'There are no products to list in this category.')]")
+    browser.find_element(By.XPATH, "// a[contains(text(), 'Continue')]").click()
+
+
+def test_catalog_windows(browser):
+    browser.find_element(By.LINK_TEXT, "Laptops & Notebooks").click()
+    browser.find_element(By.XPATH, "//a[contains(text(),'Windows (0)')]").click()
+    browser.find_element(By.XPATH, "//h2[contains(text(),'Windows')]")
+    browser.find_element(By.XPATH, "// a[contains(text(), 'Continue')]").click()
+
+
+# тесты на категорию Components
+def test_catalog_components(browser):
+    browser.find_element(By.LINK_TEXT, "Components").click()
+    browser.find_element(By.XPATH, "//a[contains(text(),'Mice and Trackballs (0)')]")
+    browser.find_element(By.XPATH, "//a[contains(text(),'Monitors (2)')]")
+    browser.find_element(By.XPATH, "//a[contains(text(),'Printers (0)')]")
+    browser.find_element(By.XPATH, "//a[contains(text(),'Scanners (0)')]")
+    browser.find_element(By.XPATH, "//a[contains(text(),'Web Cameras (0)')]")
+    browser.find_element(By.LINK_TEXT, "Show All Components").click()
+    browser.find_element(By.LINK_TEXT, "Mice and Trackballs (0)")
+    browser.find_element(By.LINK_TEXT, "Monitors (2)")
+    browser.find_element(By.LINK_TEXT, "Printers (0)")
+    browser.find_element(By.LINK_TEXT, "Scanners (0)")
+    browser.find_element(By.LINK_TEXT, "Web Cameras (0)")
+
+
+def test_catalog_tablets(browser):
+    browser.find_element(By.LINK_TEXT, "Tablets").click()
+    wait_element(browser, (By.XPATH, "//div[contains(text(),'Showing 1 to 1 of 1 (1 Pages)')]"))
+    browser.find_element(By.CSS_SELECTOR, "#list-view").click()
+
+
+# тесты на категорию Software
+def test_catalog_software(browser):
+    browser.find_element(By.LINK_TEXT, "Software").click()
+    browser.find_element(By.XPATH, "//h2[contains(text(),'Software')]")
+    browser.find_element(By.XPATH, "//p[contains(text(),'There are no products to list in this category.')]")
+    browser.find_element(By.XPATH, "// a[contains(text(), 'Continue')]").click()
+
+
+# тесты на категорию Phones & PDAs
+def test_catalog_phones(browser):
+    browser.find_element(By.LINK_TEXT, "Phones & PDAs").click()
+    browser.find_element(By.XPATH, "//div[contains(text(),'Showing 1 to 3 of 3 (1 Pages)')]")
+    browser.find_element(By.LINK_TEXT, "HTC Touch HD").click()
+    browser.find_element(By.XPATH, "//p[contains(text(),'HTC Touch - in High Definition. Watch music videos')]")
+
+
+# тесты на категорию Cameras
+def test_catalog_cameras(browser):
+    browser.find_element(By.LINK_TEXT, "Cameras").click()
+    browser.find_element(By.XPATH, "//div[contains(text(),'Showing 1 to 2 of 2 (1 Pages)')]")
+    browser.find_element(By.LINK_TEXT, "Nikon D300").click()
+    browser.find_element(By.LINK_TEXT, "Reviews (0)").click()
+    browser.find_element(By.XPATH, "//h2[contains(text(),'Write a review')]")
+    wait_element(browser, (By.CSS_SELECTOR, "#button-review")).click()
+    browser.save_screenshot("test03.png")
+    browser.find_element(By.CSS_SELECTOR, ".alert-dismissible")
+
+
+# тесты на категорию MP3 Players
+def test_catalog_mp3_players(browser):
+    browser.find_element(By.LINK_TEXT, "MP3 Players").click()
+    browser.find_element(By.XPATH, "//a[contains(text(),'test 11 (0)')]")
+    browser.find_element(By.XPATH, "//a[contains(text(),'test 12 (0)')]")
+    browser.find_element(By.XPATH, "//a[contains(text(),'test 15 (0)')]")
+    browser.find_element(By.XPATH, "//a[contains(text(),'test 16 (0)')]")
+    browser.find_element(By.XPATH, "//a[contains(text(),'test 17 (0)')]")
+    browser.find_element(By.XPATH, "//a[contains(text(),'test 18 (0)')]")
+    browser.find_element(By.XPATH, "//a[contains(text(),'test 19 (0)')]")
+    browser.find_element(By.XPATH, "//a[contains(text(),'test 20 (0)')]")
+    browser.find_element(By.XPATH, "//a[contains(text(),'test 21 (0)')]")
+    browser.find_element(By.XPATH, "//a[contains(text(),'test 22 (0)')]")
+    browser.find_element(By.XPATH, "//a[contains(text(),'test 23 (0)')]")
+    browser.find_element(By.XPATH, "//a[contains(text(),'test 24 (0)')]")
+    browser.find_element(By.XPATH, "//a[contains(text(),'test 4 (0)')]")
+    browser.find_element(By.XPATH, "//a[contains(text(),'test 5 (0)')]")
+    browser.find_element(By.XPATH, "//a[contains(text(),'test 6 (0)')]")
+    browser.find_element(By.XPATH, "//a[contains(text(),'test 7 (0)')]")
+    browser.find_element(By.XPATH, "//a[contains(text(),'test 8 (0)')]")
+    browser.find_element(By.XPATH, "//a[contains(text(),'test 9 (0)')]")
+    browser.find_element(By.LINK_TEXT, "Show All MP3 Players").click()
--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.common.by import By
 from exception_handler import wait_element
+from selenium.webdriver.support import expected_conditions as EC
 
 
 def test_check_title(browser):
@@ -8,12 +9,12 @@ def test_check_title(browser):
 
 def test_button_currency(browser):
     browser.find_element(By.CSS_SELECTOR, ".fa-caret-down").click()
-    wait_element(browser, (By.NAME, "EUR")).click()
+    browser.find_element(By.NAME, "USD").click()
 
 
 def test_button_register(browser):
     browser.find_element(By.CSS_SELECTOR, ".fa-user").click()
-    wait_element(browser, (By.XPATH, "//a[contains(text(),'Login')]")).click()
+    browser.find_element(By.XPATH, "//a[contains(text(),'Login')]").click()
 
 
 def test_button_phone(browser):
@@ -38,9 +39,35 @@ def test_search_panel(browser):
 
 def test_button_search(browser):
     browser.find_element(By.CSS_SELECTOR, ".btn-lg").click()
-    wait_element(browser, (By.XPATH, "//a[contains(text(),'Search')]")).click()
+    browser.find_element(By.XPATH, "//a[contains(text(),'Search')]").click()
 
 
-def test_search_panel(browser):
+def test_shopping_preview_button(browser):
     browser.find_element(By.CSS_SELECTOR, ".btn-inverse").click()
-    wait_element(browser, (By.XPATH, "//p[contains(text(),'Your shopping cart is empty!')]"))
+    browser.find_element(By.XPATH, "//p[contains(text(),'Your shopping cart is empty!')]").click()
+
+
+def test_upper_swiper_button(browser):
+    browser.find_element(By.XPATH,
+                         "//div [@class='slideshow swiper-viewport']/div[3]/"
+                         "div[@class='swiper-button-next']").click()
+
+
+def test_upper_swiper_button_back(browser):
+    browser.find_element(By.XPATH,
+                         "//div [@class='slideshow swiper-viewport']/div[3]/"
+                         "div[@class='swiper-button-prev']").click()
+
+
+def test_transition_to_commodity_iPhone6(browser):
+    browser.find_element(By.XPATH,
+                         "//img[@class='img-responsive' and contains(@src, '192.168.1.65:8081/image/"
+                         "cache/catalog/demo/banners/iPhone6-1140x380.jpg')]").click()
+
+
+def test_add_to_cart_button(browser):
+    browser.find_element(By.XPATH, "//span[contains(text(),'Ex Tax: $500.00')]/../../../div[3]/button[1]").click()
+    browser.find_element(By.CSS_SELECTOR, ".btn-inverse").click()
+    browser.find_element(By.CSS_SELECTOR, ".btn-danger").click()
+    browser.find_element(By.XPATH, "//body/div[@id='common-home']/div[1]")
+    browser.find_element(By.XPATH, "//button[contains(text(),'×')]").click()
