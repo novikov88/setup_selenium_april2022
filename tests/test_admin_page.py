213a9c4fdd91514c3b1a29016523d6cfdd9a61d3
Vladimir_Novikov <nvs-1988@yandex.ru>
2022-06-01 14:14:13 +0300

    рефакторинг на PageObject test_admin_page.py внесены изменения в тесты

--- tests/test_admin_page.py
+++ tests/test_admin_page.py
@@ -1,91 +1,102 @@
 """Для запуска теста использовать параметр --url http://192.168.1.65:8081/admin/
 или заменить на свой IP"""
-from selenium.webdriver.common.by import By
-from exception_handler import *
-from faker import Faker
 
-# создаем экземпляр класса Faker для генерации данных
-fake = Faker()
+from page_objects.LoginAdminPage import LoginAdminPage
+from page_objects.DashboardAdminPage import DashboardAdminPage
+from page_objects.page_elements.SuccessAlert import SuccessAlert
 
 
-def test_admin_successful_login(browser):
+def test_admin_successful_authorization(browser):
     """Тест проверяет успешную авторизацию администратора и выход и админки"""
     # поиск элемента на странице логина в админке
-    browser.find_element(By.CSS_SELECTOR, ".panel-title")
+    LoginAdminPage(browser).check_title()
     # авторизация админом страница логина
-    browser.find_element(By.CSS_SELECTOR, "#input-username").send_keys("user")
-    browser.find_element(By.CSS_SELECTOR, "#input-password").send_keys("bitnami")
-    browser.find_element(By.CSS_SELECTOR, ".btn-primary").click()
+    LoginAdminPage(browser).successful_authorization_by_admin()
     # поиск элемента на странице dashboard
-    wait_element(browser, (By.CSS_SELECTOR, "#navigation"))
-    # вылогинивание из dashboard
-    browser.find_element(By.XPATH, "//span[contains(text(),'Logout')]").click()
+    DashboardAdminPage(browser).navigation_panel_check()
+    # logout из dashboard
+    DashboardAdminPage(browser).logout()
     # поиск элемента на странице логина в админке
-    browser.find_element(By.CSS_SELECTOR, ".panel-title")
+    LoginAdminPage(browser).check_title()
+
+
+def test_add_new_product(browser):
+    """Тест проверяет успешное добавление товара в админке"""
+    # авторизация админом страница логина
+    LoginAdminPage(browser).successful_authorization_by_admin()
+    # перейти к добавлению товара
+    DashboardAdminPage(browser).go_to_section_adding_product()
+    DashboardAdminPage(browser).set_product_name()
+    DashboardAdminPage(browser).set_description()
+    DashboardAdminPage(browser).set_meta_tag()
+    DashboardAdminPage(browser).go_to_data_tab()
+    DashboardAdminPage(browser).set_model()
+    DashboardAdminPage(browser).set_price()
+    DashboardAdminPage(browser).save_product()
+    DashboardAdminPage(browser).check_product()
+
+
+def test_delete_product(browser):
+    """Тест проверяет удаление товара в админке"""
+    # авторизация админом страница логина
+    LoginAdminPage(browser).successful_authorization_by_admin()
+    # перейти ко всем товарам
+    DashboardAdminPage(browser).go_to_section_all_products()
+    # выбрать товар
+    DashboardAdminPage(browser).choose_a_product()
+    # удалить товар
+    DashboardAdminPage(browser).delete_a_product()
+    # проверка алерта с ошибкой
+    SuccessAlert(browser).check_alert()
+    # закрытие алерта
+    SuccessAlert(browser).close_alert()
 
 
 def test_admin_invalid_password(browser):
     """Тест проверяет попытку авторизации администратора с неверным паролем проверку отображения предупреждения и
     закрытие сообщения"""
     # авторизация админом страница логина
-    browser.find_element(By.CSS_SELECTOR, "#input-username").send_keys("user")
-    browser.find_element(By.CSS_SELECTOR, "#input-password").send_keys(fake.password())
-    browser.find_element(By.CSS_SELECTOR, ".btn-primary").click()
+    LoginAdminPage(browser).enter_a_valid_username()
+    LoginAdminPage(browser).enter_a_invalid_password()
+    LoginAdminPage(browser).click_on_the_button_login()
     # проверка алерта с ошибкой
-    browser.find_element(By.CSS_SELECTOR, ".alert-danger")
+    SuccessAlert(browser).check_alert()
     # закрытие алерта
-    browser.find_element(By.XPATH, "//button[contains(text(),'×')]").click()
+    SuccessAlert(browser).close_alert()
 
 
 def test_admin_invalid_login(browser):
     """Тест проверяет попытку авторизации администратора с неверным логином проверку отображения предупреждения и
     закрытие сообщения"""
     # авторизация админом страница логина
-    browser.find_element(By.CSS_SELECTOR, "#input-username").send_keys(fake.name())
-    browser.find_element(By.CSS_SELECTOR, "#input-password").send_keys("bitnami")
-    browser.find_element(By.CSS_SELECTOR, ".btn-primary").click()
+    LoginAdminPage(browser).enter_a_invalid_username()
+    LoginAdminPage(browser).enter_a_valid_password()
+    LoginAdminPage(browser).click_on_the_button_login()
     # проверка алерта с ошибкой
-    browser.find_element(By.CSS_SELECTOR, ".alert-danger")
+    SuccessAlert(browser).check_alert()
     # закрытие алерта
-    browser.find_element(By.XPATH, "//button[contains(text(),'×')]").click()
+    SuccessAlert(browser).close_alert()
 
 
 def test_admin_incorrect_username_and_password(browser):
     """Тест проверяет попытку авторизации администратора с неверным логином и паролем, проверку отображения
     предупреждения и закрытие сообщения"""
     # авторизация админом страница логина
-    browser.find_element(By.CSS_SELECTOR, "#input-username").send_keys(fake.name())
-    browser.find_element(By.CSS_SELECTOR, "#input-password").send_keys(fake.password())
-    browser.find_element(By.CSS_SELECTOR, ".btn-primary").click()
+    LoginAdminPage(browser).enter_a_invalid_username()
+    LoginAdminPage(browser).enter_a_invalid_password()
+    LoginAdminPage(browser).click_on_the_button_login()
     # проверка алерта с ошибкой
-    browser.find_element(By.CSS_SELECTOR, ".alert-danger")
+    SuccessAlert(browser).check_alert()
     # закрытие алерта
-    browser.find_element(By.XPATH, "//button[contains(text(),'×')]").click()
+    SuccessAlert(browser).close_alert()
 
 
 def test_admin_without_username_and_password(browser):
     """Тест проверяет попытку авторизации администратора без ввода логина и пароля, проверку отображения
     предупреждения и закрытие сообщения"""
     # авторизация админом страница логина
-    browser.find_element(By.CSS_SELECTOR, ".btn-primary").click()
+    LoginAdminPage(browser).click_on_the_button_login()
     # проверка алерта с ошибкой
-    browser.find_element(By.CSS_SELECTOR, ".alert-danger")
+    SuccessAlert(browser).check_alert()
     # закрытие алерта
-    browser.find_element(By.XPATH, "//button[contains(text(),'×')]").click()
-
-
-def test_admin_forgotten_password(browser):
-    """Тест проверяет кнопку восстановления пароля (forgotten password), попытку восстановить пароль на несуществующий
-    email, проверку отображения ошибки, закрытие сообщения и возврат на страницу логина"""
-    # страница логина нажатие на восстановление пароля
-    browser.find_element(By.XPATH, "//a[contains(text(),'Forgotten Password')]").click()
-    # ожидание элемента и ввод email
-    wait_element(browser, (By.XPATH, "//input[@id='input-email']")).send_keys(fake.email())
-    browser.find_element(By.XPATH, "//button[@type='submit']").click()
-    # проверка алерта с ошибкой
-    browser.find_element(By.CSS_SELECTOR, ".alert-danger")
-    browser.find_element(By.XPATH, "//button[contains(text(),'×')]").click()
-    # закрытие алерта
-    browser.find_element(By.CSS_SELECTOR, ".fa-reply").click()
-    # поиск элемента на странице логина в админке
-    browser.find_element(By.CSS_SELECTOR, ".panel-title")
+    SuccessAlert(browser).close_alert()
--- tests/test_card_product.py
+++ tests/test_card_product.py
@@ -1,142 +1,49 @@
-from selenium.webdriver.common.by import By
-from exception_handler import wait_element
-from faker import Faker
-
-# создаем экземпляр класса Faker для генерации данных
-fake = Faker()
+from page_objects.MainPage import MainPage
+from page_objects.ProductPage import ProductPage
 
 
 # тесты на карточку товара проверка радиобаттонов
-def test_apple_cinema_30_radiobuttons(browser):
+def test_apple_cinema_30_radio_buttons(browser):
     """Тесты на карточку товара проверка радиобаттонов"""
     # главная страница переход в карточку товара
-    browser.find_element(By.LINK_TEXT, 'Apple Cinema 30"').click()
+    MainPage(browser).go_to_product()
     # страница продукта клики по радиобаттонам
-    browser.find_element(By.XPATH, "//div[@id='input-option218']/div[1]/label").click()
-    browser.find_element(By.XPATH, "//div[@id='input-option218']/div[2]/label").click()
-    browser.find_element(By.XPATH, "//div[@id='input-option218']/div[3]/label").click()
+    ProductPage(browser).click_radio_buttons()
 
 
 # тесты на карточку товара проверка чекбоксов
 def test_apple_cinema_30_checkboxes(browser):
     """Тесты на карточку товара проверка чекбоксов"""
     # главная страница переход в карточку товара
-    browser.find_element(By.LINK_TEXT, 'Apple Cinema 30"').click()
+    MainPage(browser).go_to_product()
     # страница продукта активация чекбоксов
-    browser.find_element(By.XPATH, "//div[@id='input-option223']/div[1]/label").click()
-    browser.find_element(By.XPATH, "//div[@id='input-option223']/div[2]/label").click()
-    browser.find_element(By.XPATH, "//div[@id='input-option223']/div[3]/label").click()
-    browser.find_element(By.XPATH, "//div[@id='input-option223']/div[4]/label").click()
+    ProductPage(browser).activation_check_boxes()
     # страница продукта деактивация чекбоксов
-    browser.find_element(By.XPATH, "//div[@id='input-option223']/div[1]/label").click()
-    browser.find_element(By.XPATH, "//div[@id='input-option223']/div[2]/label").click()
-    browser.find_element(By.XPATH, "//div[@id='input-option223']/div[3]/label").click()
-    browser.find_element(By.XPATH, "//div[@id='input-option223']/div[4]/label").click()
+    ProductPage(browser).deactivation_check_boxes()
 
 
 # тесты на карточку товара проверка поля текст
 def test_apple_cinema_30_text_field(browser):
     """Тесты на карточку товара проверка поля текст"""
     # главная страница переход в карточку товара
-    browser.find_element(By.LINK_TEXT, 'Apple Cinema 30"').click()
-    # карточка товара отчищаем поле от дефолтного текста
-    browser.find_element(By.XPATH, "//input[@id='input-option208']").clear()
-    # карточка товара вводим текст Hello, world!
-    browser.find_element(By.XPATH, "//input[@id='input-option208']").send_keys(fake.text())
-
-
-# проверка дропдауна Select с выбором одного из значений
-def test_apple_cinema_30_select_field(browser):
-    """Проверка дропдауна Select с выбором одного из значений"""
-    # главная страница переход в карточку товара
-    browser.find_element(By.LINK_TEXT, 'Apple Cinema 30"').click()
-    # карточка товара проверка и выбор значения в дропдауне
-    browser.find_element(By.CSS_SELECTOR, "#input-option217").click()
-    browser.find_element(By.XPATH, "//option[contains(text(),'(+$4.80)')]").click()
-    # карточка товара проверка и выбор значения в дропдауне
-    browser.find_element(By.CSS_SELECTOR, "#input-option217").click()
-    browser.find_element(By.XPATH, "//option[contains(text(),'(+$3.60)')]").click()
-    # карточка товара проверка и выбор значения в дропдауне
-    browser.find_element(By.CSS_SELECTOR, "#input-option217").click()
-    browser.find_element(By.XPATH, "//option[contains(text(),'(+$1.20)')]").click()
-    # карточка товара проверка и выбор значения в дропдауне
-    browser.find_element(By.CSS_SELECTOR, "#input-option217").click()
-    browser.find_element(By.XPATH, "//option[contains(text(),'(+$2.40)')]").click()
+    MainPage(browser).go_to_product()
+    # карточка товара ввод текста в поле "Текст"
+    ProductPage(browser).entering_text_in_field_text()
 
 
 # проверка поля textarea с вводом текста
 def test_apple_cinema_30_textarea_field(browser):
     """Проверка поля textarea с вводом текста"""
     # главная страница переход в карточку товара
-    browser.find_element(By.LINK_TEXT, 'Apple Cinema 30"').click()
-    # карточка товара ввод текста в поле
-    browser.find_element(By.CSS_SELECTOR, '#input-option209').send_keys(fake.text())
-
-
-#  проверка кнопки Upload File
-def test_apple_cinema_30_upload_button(browser):
-    """Проверка кнопки Upload File"""
-    # главная страница переход в карточку товара
-    browser.find_element(By.LINK_TEXT, 'Apple Cinema 30"').click()
-    # карточка товара проверка элемента и нажатие на кнопку
-    browser.find_element(By.CSS_SELECTOR, '#button-upload222').click()
+    MainPage(browser).go_to_product()
+    # карточка товара ввод текста в поле "Textarea"
+    ProductPage(browser).entering_text_in_field_text_area()
 
 
 #  проверка поля Date с выбором даты из календаря
 def test_apple_cinema_30_date(browser):
     """Проверка поля Date с выбором даты из календаря"""
     # главная страница переход в карточку товара
-    browser.find_element(By.LINK_TEXT, 'Apple Cinema 30"').click()
+    MainPage(browser).go_to_product()
     # карточка товара ввод даты из календаря
-    browser.find_element(By.CSS_SELECTOR, '.fa-calendar').click()
-    browser.find_element(By.XPATH, '//tbody/tr[2]/td').click()
-
-
-#  проверка поля Date с вводом времени
-def test_apple_cinema_30_time(browser):
-    """Проверка поля Date с вводом времени"""
-    # главная страница переход в карточку товара
-    browser.find_element(By.LINK_TEXT, 'Apple Cinema 30"').click()
-    # карточка товара ввод даты и времени
-    browser.find_element(By.CSS_SELECTOR, '#input-option221').clear()
-    browser.find_element(By.CSS_SELECTOR, '#input-option221').send_keys('23:59')
-    # карточка товара закрытие календаря
-    browser.find_element(By.XPATH, "//body").click()
-
-
-#  проверка поля Date & Time с вводом даты и времени
-def test_apple_cinema_30_data_time(browser):
-    """Проверка поля Date & Time с вводом даты и времени"""
-    # главная страница переход в карточку товара
-    browser.find_element(By.LINK_TEXT, 'Apple Cinema 30"').click()
-    # карточка товара ввод даты и времени
-    browser.find_element(By.CSS_SELECTOR, '#input-option220').clear()
-    browser.find_element(By.CSS_SELECTOR, '#input-option220').send_keys('2020-05-15 15:15')
-    browser.find_element(By.XPATH, "//body").click()
-
-
-#  проверка поля Qty с вводом значения
-def test_apple_cinema_30_qty(browser):
-    """Проверка поля Qty с вводом значения"""
-    # главная страница переход в карточку товара
-    browser.find_element(By.LINK_TEXT, 'Apple Cinema 30"').click()
-    # карточка товара ввод значений в поле Qty
-    browser.find_element(By.CSS_SELECTOR, '#input-quantity').clear()
-    browser.find_element(By.CSS_SELECTOR, '#input-quantity').send_keys('+100500')
-    browser.find_element(By.XPATH, "//body").click()
-
-
-#  поиск кнопки Add to Cart с проверкой сообщений об обязательности полей после нажатия
-def test_apple_cinema_30_add_to_cart(browser):
-    """Поиск кнопки Add to Cart с проверкой сообщений об обязательности полей после нажатия"""
-    # главная страница переход в карточку товара
-    browser.find_element(By.LINK_TEXT, 'Apple Cinema 30"').click()
-    # карточка товара нажатие на кнопку
-    browser.find_element(By.CSS_SELECTOR, '#button-cart').click()
-    # карточка товара ожидание элементов и проверка предупреждений
-    wait_element(browser, (By.XPATH, "//div[contains(text(),'Textarea required!')]"))
-    # карточка товара ищем через find_element так как ожидание уже прошло в строчке выше
-    browser.find_element(By.XPATH, "//div[contains(text(),'Radio required!')]")
-    browser.find_element(By.XPATH, "//div[contains(text(),'Checkbox required!')]")
-    browser.find_element(By.XPATH, "//div[contains(text(),'Select required!')]")
-    browser.find_element(By.XPATH, "//div[contains(text(),'File required!')]")
+    ProductPage(browser).select_day_in_date_calendar()
